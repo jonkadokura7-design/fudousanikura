3e3c0a7284e64cff4e6eb32dfa278d401bd20261
webdesigner_tk <jonkadokura7@gmail.com>
2026-01-23 02:05:51 +0900

    Add files via upload

---
+++ js/data/all_cities.js
@@ -0,0 +1,163 @@
+// Comprehensive Fallback City Data (JIS Codes)
+// Used when MLIT API is unreachable (Offline/Demo Mode)
+
+window.ALL_CITIES_MAP = {
+    // --- Hokkaido ---
+    "01": [
+        { id: "01100", name: "札幌市 (Sapporo)" },
+        { id: "01101", name: "札幌市中央区 (Chuo-ku)" }, { id: "01102", name: "札幌市北区 (Kita-ku)" },
+        { id: "01103", name: "札幌市東区 (Higashi-ku)" }, { id: "01104", name: "札幌市白石区 (Shiroishi-ku)" },
+        { id: "01105", name: "札幌市豊平区 (Toyohira-ku)" }, { id: "01106", name: "札幌市南区 (Minami-ku)" },
+        { id: "01107", name: "札幌市西区 (Nishi-ku)" }, { id: "01108", name: "札幌市厚別区 (Atsubetsu-ku)" },
+        { id: "01109", name: "札幌市手稲区 (Teine-ku)" }, { id: "01110", name: "札幌市清田区 (Kiyota-ku)" },
+        { id: "01202", name: "函館市 (Hakodate)" }, { id: "01203", name: "小樽市 (Otaru)" },
+        { id: "01204", name: "旭川市 (Asahikawa)" }, { id: "01205", name: "室蘭市 (Muroran)" },
+        { id: "01206", name: "釧路市 (Kushiro)" }, { id: "01207", name: "帯広市 (Obihiro)" },
+        { id: "01208", name: "北見市 (Kitami)" }, { id: "01209", name: "夕張市 (Yubari)" },
+        { id: "01210", name: "岩見沢市 (Iwamizawa)" }, { id: "01211", name: "網走市 (Abashiri)" },
+        { id: "01212", name: "留萌市 (Rumoi)" }, { id: "01213", name: "苫小牧市 (Tomakomai)" },
+        { id: "01214", name: "稚内市 (Wakkanai)" }, { id: "01215", name: "美唄市 (Bibai)" },
+        { id: "01216", name: "芦別市 (Ashibetsu)" }, { id: "01217", name: "江別市 (Ebetsu)" },
+        { id: "01224", name: "千歳市 (Chitose)" }
+    ],
+    // --- Tohoku ---
+    "02": [{ id: "02201", name: "青森市 (Aomori)" }, { id: "02202", name: "弘前市 (Hirosaki)" }, { id: "02203", name: "八戸市 (Hachinohe)" }],
+    "03": [{ id: "03201", name: "盛岡市 (Morioka)" }, { id: "03202", name: "宮古市 (Miyako)" }, { id: "03203", name: "大船渡市 (Ofunato)" }],
+    "04": [{ id: "04100", name: "仙台市 (Sendai)" }, { id: "04101", name: "仙台市青葉区 (Aoba-ku)" }, { id: "04102", name: "仙台市宮城野区 (Miyagino-ku)" }, { id: "04202", name: "石巻市 (Ishinomaki)" }],
+    "05": [{ id: "05201", name: "秋田市 (Akita)" }, { id: "05202", name: "能代市 (Noshiro)" }, { id: "05203", name: "横手市 (Yokote)" }],
+    "06": [{ id: "06201", name: "山形市 (Yamagata)" }, { id: "06202", name: "米沢市 (Yonezawa)" }, { id: "06203", name: "鶴岡市 (Tsuruoka)" }],
+    "07": [{ id: "07201", name: "福島市 (Fukushima)" }, { id: "07202", name: "会津若松市 (Aizuwakamatsu)" }, { id: "07203", name: "郡山市 (Koriyama)" }, { id: "07204", name: "いわき市 (Iwaki)" }],
+
+    // --- Kanto ---
+    "08": [ // Ibaraki
+        { id: "08201", name: "水戸市 (Mito)" }, { id: "08202", name: "日立市 (Hitachi)" },
+        { id: "08203", name: "土浦市 (Tsuchiura)" }, { id: "08204", name: "古河市 (Koga)" },
+        { id: "08205", name: "石岡市 (Ishioka)" }, { id: "08220", name: "つくば市 (Tsukuba)" }
+    ],
+    "09": [ // Tochigi
+        { id: "09201", name: "宇都宮市 (Utsunomiya)" }, { id: "09202", name: "足利市 (Ashikaga)" },
+        { id: "09203", name: "栃木市 (Tochigi)" }, { id: "09204", name: "佐野市 (Sano)" }
+    ],
+    "10": [ // Gunma
+        { id: "10201", name: "前橋市 (Maebashi)" }, { id: "10202", name: "高崎市 (Takasaki)" },
+        { id: "10203", name: "桐生市 (Kiryu)" }, { id: "10204", name: "伊勢崎市 (Isesaki)" }
+    ],
+    "11": [ // Saitama
+        { id: "11100", name: "さいたま市 (Saitama)" },
+        { id: "11101", name: "さいたま市西区 (Nishi-ku)" }, { id: "11102", name: "さいたま市北区 (Kita-ku)" },
+        { id: "11103", name: "さいたま市大宮区 (Omiya-ku)" }, { id: "11104", name: "さいたま市見沼区 (Minuma-ku)" },
+        { id: "11105", name: "さいたま市中央区 (Chuo-ku)" }, { id: "11106", name: "さいたま市桜区 (Sakura-ku)" },
+        { id: "11107", name: "さいたま市浦和区 (Urawa-ku)" }, { id: "11108", name: "さいたま市南区 (Minami-ku)" },
+        { id: "11109", name: "さいたま市緑区 (Midori-ku)" }, { id: "11110", name: "さいたま市岩槻区 (Iwatsuki-ku)" },
+        { id: "11201", name: "川越市 (Kawagoe)" }, { id: "11202", name: "熊谷市 (Kumagaya)" },
+        { id: "11203", name: "川口市 (Kawaguchi)" }, { id: "11204", name: "行田市 (Gyoda)" },
+        { id: "11205", name: "秩父市 (Chichibu)" }, { id: "11206", name: "所沢市 (Tokorozawa)" },
+        { id: "11208", name: "加須市 (Kazo)" }, { id: "11209", name: "本庄市 (Honjo)" },
+        { id: "11210", name: "東松山市 (Higashimatsuyama)" }, { id: "11211", name: "春日部市 (Kasukabe)" },
+        { id: "11212", name: "狭山市 (Sayama)" }, { id: "11214", name: "草加市 (Soka)" },
+        { id: "11215", name: "越谷市 (Koshigaya)" }, { id: "11219", name: "上尾市 (Ageo)" }
+    ],
+    "12": [ // Chiba
+        { id: "12100", name: "千葉市 (Chiba)" },
+        { id: "12101", name: "千葉市中央区 (Chuo-ku)" }, { id: "12102", name: "千葉市花見川区 (Hanamigawa-ku)" },
+        { id: "12103", name: "千葉市稲毛区 (Inage-ku)" }, { id: "12104", name: "千葉市若葉区 (Wakaba-ku)" },
+        { id: "12105", name: "千葉市緑区 (Midori-ku)" }, { id: "12106", name: "千葉市美浜区 (Mihama-ku)" },
+        { id: "12202", name: "銚子市 (Choshi)" }, { id: "12203", name: "市川市 (Ichikawa)" },
+        { id: "12204", name: "船橋市 (Funabashi)" }, { id: "12205", name: "館山市 (Tateyama)" },
+        { id: "12206", name: "木更津市 (Kisarazu)" }, { id: "12207", name: "松戸市 (Matsudo)" },
+        { id: "12208", name: "野田市 (Noda)" }, { id: "12210", name: "茂原市 (Mobara)" },
+        { id: "12211", name: "成田市 (Narita)" }, { id: "12212", name: "佐倉市 (Sakura)" },
+        { id: "12217", name: "柏市 (Kashiwa)" }, { id: "12219", name: "市原市 (Ichihara)" },
+        { id: "12221", name: "八千代市 (Yachiyo)" }, { id: "12227", name: "浦安市 (Urayasu)" }
+    ],
+    "13": [ // Tokyo (All Wards + Major Cities)
+        { id: "13101", name: "千代田区 (Chiyoda-ku)" }, { id: "13102", name: "中央区 (Chuo-ku)" },
+        { id: "13103", name: "港区 (Minato-ku)" }, { id: "13104", name: "新宿区 (Shinjuku-ku)" },
+        { id: "13105", name: "文京区 (Bunkyo-ku)" }, { id: "13106", name: "台東区 (Taito-ku)" },
+        { id: "13107", name: "墨田区 (Sumida-ku)" }, { id: "13108", name: "江東区 (Koto-ku)" },
+        { id: "13109", name: "品川区 (Shinagawa-ku)" }, { id: "13110", name: "目黒区 (Meguro-ku)" },
+        { id: "13111", name: "大田区 (Ota-ku)" }, { id: "13112", name: "世田谷区 (Setagaya-ku)" },
+        { id: "13113", name: "渋谷区 (Shibuya-ku)" }, { id: "13114", name: "中野区 (Nakano-ku)" },
+        { id: "13115", name: "杉並区 (Suginami-ku)" }, { id: "13116", name: "豊島区 (Toshima-ku)" },
+        { id: "13117", name: "北区 (Kita-ku)" }, { id: "13118", name: "荒川区 (Arakawa-ku)" },
+        { id: "13119", name: "板橋区 (Itabashi-ku)" }, { id: "13120", name: "練馬区 (Nerima-ku)" },
+        { id: "13121", name: "足立区 (Adachi-ku)" }, { id: "13122", name: "葛飾区 (Katsushika-ku)" },
+        { id: "13123", name: "江戸川区 (Edogawa-ku)" },
+        { id: "13201", name: "八王子市 (Hachioji)" }, { id: "13202", name: "立川市 (Tachikawa)" },
+        { id: "13203", name: "武蔵野市 (Musashino)" }, { id: "13204", name: "三鷹市 (Mitaka)" },
+        { id: "13205", name: "青梅市 (Ome)" }, { id: "13206", name: "府中市 (Fuchu)" },
+        { id: "13209", name: "町田市 (Machida)" }, { id: "13224", name: "多摩市 (Tama)" }
+    ],
+    "14": [ // Kanagawa (All Cities)
+        { id: "14100", name: "横浜市 (Yokohama)" },
+        { id: "14101", name: "横浜市鶴見区 (Tsurumi-ku)" }, { id: "14103", name: "横浜市西区 (Nishi-ku)" },
+        { id: "14104", name: "横浜市中区 (Naka-ku)" }, { id: "14108", name: "横浜市金沢区 (Kanazawa-ku)" },
+        { id: "14109", name: "横浜市港北区 (Kohoku-ku)" }, { id: "14110", name: "横浜市戸塚区 (Totsuka-ku)" },
+        { id: "14130", name: "川崎市 (Kawasaki)" },
+        { id: "14131", name: "川崎市川崎区 (Kawasaki-ku)" }, { id: "14132", name: "川崎市幸区 (Saiwai-ku)" },
+        { id: "14133", name: "川崎市中原区 (Nakahara-ku)" }, { id: "14137", name: "川崎市麻生区 (Asao-ku)" },
+        { id: "14150", name: "相模原市 (Sagamihara)" },
+        { id: "14201", name: "横須賀市 (Yokosuka)" }, { id: "14203", name: "平塚市 (Hiratsuka)" },
+        { id: "14204", name: "鎌倉市 (Kamakura)" }, { id: "14205", name: "藤沢市 (Fujisawa)" },
+        { id: "14206", name: "小田原市 (Odawara)" }, { id: "14207", name: "茅ヶ崎市 (Chigasaki)" },
+        { id: "14208", name: "逗子市 (Zushi)" }, { id: "14210", name: "三浦市 (Miura)" },
+        { id: "14212", name: "厚木市 (Atsugi)" }, { id: "14213", name: "大和市 (Yamato)" },
+        { id: "14215", name: "海老名市 (Ebina)" }, { id: "14216", name: "座間市 (Zama)" }
+    ],
+
+    // --- Chubu ---
+    "15": [{ id: "15100", name: "新潟市 (Niigata)" }, { id: "15202", name: "長岡市 (Nagaoka)" }],
+    "16": [{ id: "16201", name: "富山市 (Toyama)" }, { id: "16202", name: "高岡市 (Takaoka)" }],
+    "17": [{ id: "17201", name: "金沢市 (Kanazawa)" }, { id: "17202", name: "七尾市 (Nanao)" }],
+    "18": [{ id: "18201", name: "福井市 (Fukui)" }, { id: "18202", name: "敦賀市 (Tsuruga)" }],
+    "19": [{ id: "19201", name: "甲府市 (Kofu)" }, { id: "19202", name: "富士吉田市 (Fujiyoshida)" }],
+    "20": [{ id: "20201", name: "長野市 (Nagano)" }, { id: "20202", name: "松本市 (Matsumoto)" }, { id: "20203", name: "上田市 (Ueda)" }],
+    "21": [{ id: "21201", name: "岐阜市 (Gifu)" }, { id: "21202", name: "大垣市 (Ogaki)" }, { id: "21203", name: "高山市 (Takayama)" }],
+    "22": [{ id: "22100", name: "静岡市 (Shizuoka)" }, { id: "22130", name: "浜松市 (Hamamatsu)" }, { id: "22203", name: "沼津市 (Numazu)" }, { id: "22205", name: "熱海市 (Atami)" }],
+    "23": [ // Aichi (Expanded)
+        { id: "23100", name: "名古屋市 (Nagoya)" },
+        { id: "23101", name: "名古屋市千種区 (Chikusa-ku)" }, { id: "23106", name: "名古屋市中区 (Naka-ku)" },
+        { id: "23201", name: "豊橋市 (Toyohashi)" }, { id: "23202", name: "岡崎市 (Okazaki)" },
+        { id: "23203", name: "一宮市 (Ichinomiya)" }, { id: "23206", name: "春日井市 (Kasugai)" },
+        { id: "23211", name: "豊田市 (Toyota)" }, { id: "23212", name: "安城市 (Anjo)" }
+    ],
+
+    // --- Kansai ---
+    "24": [{ id: "24201", name: "津市 (Tsu)" }, { id: "24202", name: "四日市市 (Yokkaichi)" }, { id: "24203", name: "伊勢市 (Ise)" }],
+    "25": [{ id: "25201", name: "大津市 (Otsu)" }, { id: "25202", name: "彦根市 (Hikone)" }],
+    "26": [{ id: "26100", name: "京都市 (Kyoto)" }, { id: "26102", name: "京都市上京区 (Kamigyo-ku)" }, { id: "26201", name: "福知山市 (Fukuchiyama)" }, { id: "26204", name: "宇治市 (Uji)" }],
+    "27": [ // Osaka (Expanded)
+        { id: "27100", name: "大阪市 (Osaka)" },
+        { id: "27104", name: "大阪市西区 (Nishi-ku)" }, { id: "27109", name: "大阪市中央区 (Chuo-ku)" },
+        { id: "27127", name: "大阪市北区 (Kita-ku)" }, { id: "27128", name: "大阪市中央区 (Chuo-ku)" },
+        { id: "27140", name: "堺市 (Sakai)" },
+        { id: "27203", name: "豊中市 (Toyonaka)" }, { id: "27204", name: "池田市 (Ikeda)" },
+        { id: "27205", name: "吹田市 (Suita)" }, { id: "27207", name: "高槻市 (Takatsuki)" },
+        { id: "27210", name: "枚方市 (Hirakata)" }, { id: "27211", name: "茨木市 (Ibaraki)" },
+        { id: "27212", name: "八尾市 (Yao)" }, { id: "27227", name: "東大阪市 (Higashiosaka)" }
+    ],
+    "28": [{ id: "28100", name: "神戸市 (Kobe)" }, { id: "28101", name: "神戸市東灘区 (Higashinada-ku)" }, { id: "28201", name: "姫路市 (Himeji)" }, { id: "28202", name: "尼崎市 (Amagasaki)" }, { id: "28204", name: "西宮市 (Nishinomiya)" }, { id: "28206", name: "芦屋市 (Ashiya)" }],
+    "29": [{ id: "29201", name: "奈良市 (Nara)" }, { id: "29202", name: "大和高田市 (Yamatotakada)" }],
+    "30": [{ id: "30201", name: "和歌山市 (Wakayama)" }],
+
+    // --- Chugoku/Shikoku ---
+    "31": [{ id: "31201", name: "鳥取市 (Tottori)" }],
+    "32": [{ id: "32201", name: "松江市 (Matsue)" }],
+    "33": [{ id: "33100", name: "岡山市 (Okayama)" }, { id: "33202", name: "倉敷市 (Kurashiki)" }],
+    "34": [{ id: "34100", name: "広島市 (Hiroshima)" }, { id: "34202", name: "呉市 (Kure)" }, { id: "34207", name: "福山市 (Fukuyama)" }],
+    "35": [{ id: "35201", name: "下関市 (Shimonoseki)" }, { id: "35203", name: "山口市 (Yamaguchi)" }],
+    "36": [{ id: "36201", name: "徳島市 (Tokushima)" }],
+    "37": [{ id: "37201", name: "高松市 (Takamatsu)" }],
+    "38": [{ id: "38201", name: "松山市 (Matsuyama)" }],
+    "39": [{ id: "39201", name: "高知市 (Kochi)" }],
+
+    // --- Kyushu ---
+    "40": [{ id: "40100", name: "福岡市 (Fukuoka)" }, { id: "40130", name: "北九州市 (Kitakyushu)" }, { id: "40203", name: "久留米市 (Kurume)" }],
+    "41": [{ id: "41201", name: "佐賀市 (Saga)" }],
+    "42": [{ id: "42201", name: "長崎市 (Nagasaki)" }, { id: "42202", name: "佐世保市 (Sasebo)" }],
+    "43": [{ id: "43100", name: "熊本市 (Kumamoto)" }],
+    "44": [{ id: "44201", name: "大分市 (Oita)" }, { id: "44202", name: "別府市 (Beppu)" }],
+    "45": [{ id: "45201", name: "宮崎市 (Miyazaki)" }],
+    "46": [{ id: "46201", name: "鹿児島市 (Kagoshima)" }],
+    "47": [{ id: "47201", name: "那覇市 (Naha)" }, { id: "47207", name: "石垣市 (Ishigaki)" }, { id: "47211", name: "沖縄市 (Okinawa)" }]
+};
